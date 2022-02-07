3871c153e7eb79eb5787b59266f39431b84d1d86
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-02-07 21:04:06 +0530

    Used Axios to send GET Request to TMDB API

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "../banner.jpg";
+import axios from "axios";
+import { Oval } from "react-loader-spinner";
 
 function Movies() {
+ 
+ const [movies, setMovies] = useState([]);
+
+ axios.get("https://api.themoviedb.org/3/trending/movie/week?api_key=5984df98ddb29f07afe65af3db9ac90c").then((res)=> console.table(res.data.results));   
+
   return <>
     <div className='mb-8 z-1'>
         <div className='mt-8 mb-10 font-bold text-2xl text-center '>Trending Movies</div>
-        <div className='flex flex-wrap justify-center '>
-            <div className={`bg-[url(${Image})] h-[25vh] w-[150px]  md:h-[30vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
-                <div className='w-full bg-gray-900 text-white py-2 text-center rounded-b-xl font-bold '>Title</div>
+        {
+            movies.length == 0 ? 
+                <div className='flex justify-center'>
+                    <Oval
+                    heigth="100"
+                    width="100"
+                    color='grey'
+                    secondaryColor="grey"
+                    ariaLabel='loading'
+                    />  
+                </div>  
+                                :   
 
-            </div>
-            <div className={`bg-[url(${Image})] h-[25vh] w-[150px]  md:h-[30vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
-                <div className='w-full bg-gray-900 text-white py-2 text-center rounded-b-xl font-bold '>Title</div>
+                    <div className='flex flex-wrap justify-center '>
+                        <div className={`bg-[url(${Image})] h-[25vh] w-[150px]  md:h-[30vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
+                            <div className='w-full bg-gray-900 text-white py-2 text-center rounded-b-xl font-bold '>Title</div>
 
-            </div>
-            <div className={`bg-[url(${Image})] h-[25vh] w-[150px]  md:h-[30vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
-                <div className='w-full bg-gray-900 text-white py-2 text-center rounded-b-xl font-bold '>Title</div>
+                        </div>
+                        <div className={`bg-[url(${Image})] h-[25vh] w-[150px]  md:h-[30vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
+                            <div className='w-full bg-gray-900 text-white py-2 text-center rounded-b-xl font-bold '>Title</div>
 
-            </div>
-            <div className={`bg-[url(${Image})] h-[25vh] w-[150px]  md:h-[30vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
-                <div className='w-full bg-gray-900 text-white py-2 text-center rounded-b-xl font-bold '>Title</div>
+                        </div>
+                        <div className={`bg-[url(${Image})] h-[25vh] w-[150px]  md:h-[30vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
+                            <div className='w-full bg-gray-900 text-white py-2 text-center rounded-b-xl font-bold '>Title</div>
 
-            </div>
-        </div>
+                        </div>
+                        <div className={`bg-[url(${Image})] h-[25vh] w-[150px]  md:h-[30vh] md:w-[200px] bg-cover bg-center rounded-xl flex items-end m-4 hover:scale-110 ease-out duration-300`}>
+                            <div className='w-full bg-gray-900 text-white py-2 text-center rounded-b-xl font-bold '>Title</div>
+
+                        </div>
+                    </div>
+
+        }
     </div>
   </>;
 }
