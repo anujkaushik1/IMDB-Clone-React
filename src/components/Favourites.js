f25d1a49497d8e9f194a9b50a8b9350ad4ceed20
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-02-08 11:00:20 +0530

    Made static buttons in Favourites UI

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function Favourites() {
-  return <div>Favourite Component</div>;
+  return <>
+
+    <div className=' flex justify-center flex-wrap space-x-2 mt-4'>
+
+      <button className='text-lg p-1 px-2 bg-yellow-600 text-white rounded-xl font-bold'>
+        All Genres 
+      </button>
+
+      <button className='text-lg p-1 px-2 bg-gray-400 hover:bg-yellow-600 text-white rounded-xl font-bold'>
+        All Genres 
+      </button>
+      
+    </div>
+  
+  </>;
 }
 
 export default Favourites;
